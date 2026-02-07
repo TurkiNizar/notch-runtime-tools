4ae757567bd05535c812f20296e4934117f0d9e0
nturki <turki.nizar@yahoo.fr>
2026-02-07 20:23:24 +0100

    Run app as agent; ignore user data

--- NotchRuntimeToolsForDevs/NotchRuntimeToolsForDevs/NotchRuntimeToolsForDevsApp.swift
+++ NotchRuntimeToolsForDevs/NotchRuntimeToolsForDevs/NotchRuntimeToolsForDevsApp.swift
@@ -1,19 +1,11 @@
-//
-//  NotchRuntimeToolsForDevsApp.swift
-//  NotchRuntimeToolsForDevs
-//
-//  Created by Nizar TURKI on 06/02/2026.
-//
-
 import SwiftUI
 
 @main
 struct NotchRuntimeToolsForDevsApp: App {
     @StateObject private var coordinator = NotchCoordinator()
-
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
                 .environmentObject(coordinator)
         }
     }
